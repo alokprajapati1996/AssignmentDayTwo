3f272de0d60aa922b146995866ba708032c31b86
Alok kumar prajapati <alokprajapati913@gmail.com>
2023-05-17 12:14:44 +0530

    Added:code reverse number use for loop

--- src/DayTwoProgram.java
+++ src/DayTwoProgram.java
@@ -84,7 +84,18 @@ public static void sumNaturalNumberWithForLoop(){
     }
     System.out.println("sum of natural number : "+sum);
 }
-
+public static void reverseUsedWithFor(){
+    Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a value");
+    int num=sc.nextInt();
+    int sum=0;
+        while(num>0){
+        int rem=num%10;
+        sum=sum*10+rem;
+        num=num/10;
+    }
+        System.out.println("revese no. is :"+sum);
+}
 
 public static void main(String[] args) {
         digitNumber();
@@ -92,6 +103,7 @@ public static void main(String[] args) {
         sumNaturalNumber();
         printRevers();
     sumNaturalNumberWithForLoop();
+    reverseUsedWithFor();
 
     }
 }
