ca48b0d50dacd5d1c589476ca59c7561a4f98a2a
Alok kumar prajapati <alokprajapati913@gmail.com>
2023-05-17 11:13:24 +0530

    Added:code single digit number

--- src/DayTwoProgram.java
+++ src/DayTwoProgram.java
@@ -5,7 +5,7 @@ public class DayTwoProgram {
         System.out.println("Read a Number 1,10,100,1000 and display unit, ten hundred");
         System.out.println("Enter number");
         int num=sc.nextInt();
-        if(num==1){
+        if(num== 1){
             System.out.println("One");
         }
         else if(num==10){
@@ -20,8 +20,38 @@ public class DayTwoProgram {
             System.out.println("enter wrong value");
         }
     }
-
-    public static void main(String[] args) {
+    public static void readSingleDigit(){
+            System.out.println("Enter single Digit number 1 to 9");
+            Scanner sc=new Scanner(System.in);
+    int num=sc.nextInt();
+            if(num==1){
+        System.out.println("One");
+    }
+            else if(num==2){
+        System.out.println("Two");
+    }
+            else if(num==3){
+        System.out.println("Three");
+    }
+            else if(num==4){
+        System.out.println("Four");
+    }else if(num==5){
+        System.out.println("Five");
+    }else if(num==6){
+        System.out.println("Six");
+    }else if(num==7){
+        System.out.println("Seven");
+    }else if(num==8){
+        System.out.println("Eight");
+    }
+            else if(num==9){
+        System.out.println("Nine");
+    }else{
+        System.out.println("enter wrong value");
+    }
+}
+public static void main(String[] args) {
         digitNumber();
+        readSingleDigit();
     }
 }
