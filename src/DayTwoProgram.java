159a311b995d3e7df2134a2cbb0a65292e875fc6
Alok kumar prajapati <alokprajapati913@gmail.com>
2023-05-17 10:33:50 +0530

    Added:code singal digit number

--- src/DayTwoProgram.java
+++ src/DayTwoProgram.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
 public class DayTwoProgram {
-    public static void main(String[] args) {
+       public static void digitNumber(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Read a Number 1,10,100,1000 and display unit, ten hundred");
+        System.out.println("Enter number");
+        int num=sc.nextInt();
+        if(num==1){
+            System.out.println("One");
+        }
+        else if(num==10){
+            System.out.println("Ten");
+        }
+        else if(num==100){
+            System.out.println("One Hundred");
+        }
+        else if(num==100){
+            System.out.println("Ten Hundred");
+        } else{
+            System.out.println("enter wrong value");
+        }
+    }
 
-        System.out.println("Hello world!");
+    public static void main(String[] args) {
+        digitNumber();
     }
 }
