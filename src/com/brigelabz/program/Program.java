e007921e7d2b86d39141b321ae1eab4393e85024
Alok kumar prajapati <alokprajapati913@gmail.com>
2023-05-20 11:03:07 +0530

    Added:code check vowelsand consonant

---
+++ src/com/brigelabz/program/Program.java
@@ -0,0 +1,45 @@
+package com.brigelabz.program;
+
+public class Program {
+    public static void checkVowels() {
+        char ch = 'O';
+        switch (ch) {
+            case 'a':
+                System.out.println("Vowel");
+                break;
+            case 'e':
+                System.out.println("Vowel");
+                break;
+            case 'i':
+                System.out.println("Vowel");
+                break;
+            case 'o':
+                System.out.println("Vowel");
+                break;
+            case 'u':
+                System.out.println("Vowel");
+                break;
+            case 'A':
+                System.out.println("Vowel");
+                break;
+            case 'E':
+                System.out.println("Vowel");
+                break;
+            case 'I':
+                System.out.println("Vowel");
+                break;
+            case 'O':
+                System.out.println("Vowel");
+                break;
+            case 'U':
+                System.out.println("Vowel");
+                break;
+            default:
+                System.out.println("Consonant");
+        }
+    }
+
+public static void main(String[] args) {
+checkVowels();
+    }
+}
